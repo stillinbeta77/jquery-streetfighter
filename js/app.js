619656310681ff291d52fff728a28ab1bf9e9c47
stillinbeta77 <info@savageonlinemarketing.com>
2015-02-11 15:27:04 -0700

    added hodouken sound and click events

--- js/app.js
+++ js/app.js
@@ -8,14 +8,27 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		// play hadouken sound
+		playHadouken ();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// animate hadouken to the right of the screen
+		$('.hadouken').finish().show()
+		  .animate(
+			{'left': '300px'},
+			1000,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);			
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
